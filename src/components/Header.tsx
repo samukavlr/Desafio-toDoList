5e3f37b29846233f7d93faddf4618a076d83d1c9
SAMUEL MARCIANO <samuel.marciano@senacsp.edu.br>
2022-10-31 22:26:41 -0300

    Desafio iniciado

---
+++ src/components/Header.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import logoImg from '../assets/images/logo/logotodo.png';
+
+interface HeaderProps {
+  tasksCounter: number;
+}
+
+export function Header({ tasksCounter }: HeaderProps) {
+  const tasksCounterText = tasksCounter === 1 ?'tarefa' : 'tarefas'
+  
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.img}/>
+      
+      <View style={styles.tasks}>
+      </View>
+        {/* <Text style={styles.tasksCounter}>Você tem </Text>
+        <Text style={styles.tasksCounterBold}>{tasksCounter} {tasksCounterText}</Text> */}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height:173,
+    width:'100%',
+    backgroundColor: '#0d0d0d',
+    paddingTop:70,
+    paddingBottom:70,
+    alignItems: 'center',
+    
+  },
+
+  img: {
+    width:110.34,
+    height:32,
+
+  },
+  tasks: {
+    alignItems: 'center',
+    
+  },
+  tasksCounter: {
+    fontSize: 15,
+    color: '#FFF',
+    fontFamily: 'Inter_400Regular',
+  },
+  tasksCounterBold: {
+    fontSize: 15,
+    color: '#FFF',
+    fontFamily: 'Inter_700Bold',
+  }
+});
---
+++ src/pages/Home.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Header } from '../components/Header';
+import { Task, TasksList } from '../components/TasksList';
+import { TodoInput } from '../components/TodoInput';
+
+export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  // console.log(tasks.length)
+
+  function handleAddTask(newTaskTitle: string) {
+    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldTasks => [...oldTasks,newTask])
+  }
+
+  function handleToggleTaskDone(id: number) {
+    //TODO - toggle task done if exists
+    const updatedTask = tasks.map(task => ({ ...task}))
+
+    const foundItem = updatedTask.find(item => item.id ===id)
+    if(!foundItem)
+
+    return; 
+    foundItem.done =!foundItem.done
+    setTasks(updatedTask)
+  }
+
+  function handleRemoveTask(id: number) {
+    //TODO - remove task from state
+    const updatedTask = tasks.filter(task => task.id !== id)
+    setTasks(updatedTask)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header tasksCounter={tasks.length} />
+
+      <TodoInput addTask={handleAddTask} />
+
+      <TasksList 
+        tasks={tasks} 
+        toggleTaskDone={handleToggleTaskDone}
+        removeTask={handleRemoveTask} 
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBEBEB'
+  }
+})
